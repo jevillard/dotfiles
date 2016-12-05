ef917bc08db9d4678303ab55ea97eaf5a72a3f80
Jérémy VILLARD <jeremy.villard@smile.fr>
2016-12-05 16:56:36 +0100

    Fix typo for symfony installation

--- bin/symfony.sh
+++ bin/symfony.sh
@@ -3,6 +3,6 @@
 BASEDIR=$(dirname $0)
 . $BASEDIR/../functions.sh
 
-explain "Composer installation"
+explain "Symfony installation"
     tell sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
     tell sudo chmod a+x /usr/local/bin/symfony
